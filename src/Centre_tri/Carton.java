bf98d63cb4427d93c5d069e93f8697c835ee6ca1
solene abbas <thunderstom6a7@gmail.com>
2023-04-07 18:56:01 +0200

    methode enregistrerStats

--- src/Centre_tri/Carton.java
+++ src/Centre_tri/Carton.java
@@ -5,9 +5,10 @@ public class Carton extends Dechet{
 public final float pc;
 	
 	public Carton(float pc) {
-		this.pc = pc;}
+		this.pc = pc;
 
 	}
 	public String getType() {
 		return "carton";
+	}
 }

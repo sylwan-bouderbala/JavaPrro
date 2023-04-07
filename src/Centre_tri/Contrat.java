5ba3b9df705ad0f0c4feade6cc5d2f1750e4c644
sylwan-bouderbala <sylwanbouderbala@outlook.fr>
2023-04-07 21:37:19 +0200

    ajout contrat

--- src/Centre_tri/Contrat.java
+++ src/Centre_tri/Contrat.java
@@ -1,7 +1,61 @@
 package Centre_tri;
 
-public class Contrat implements java.io.Serializable{
-	private Commerce commerce;
-	public String Name;
+import java.time.Duration;
+import java.util.Date;
+
+public class Contrat {
+	private Commerce Commerce;
+	private String Name;
+	private Date DateDebut;
+	private Date DateFin;
+	private Duration Duree;
+	
+	public void Contrat(String Name,Commerce Commerce,Date DateDebut,Date DateFin,Duration Duree){
+		this.setCommerce(Commerce);
+		this.setName(Name);
+		this.setDateDebut(DateDebut);
+		this.setDateFin(DateFin);
+		this.setDuree(Duree);
+	}
+
+	public Duration getDuree() {
+		return Duree;
+	}
+
+	public void setDuree(Duration duree) {
+		Duree = duree;
+	}
+
+	public Date getDateFin() {
+		return DateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		DateFin = dateFin;
+	}
+
+	public Date getDateDebut() {
+		return DateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		DateDebut = dateDebut;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public Commerce getCommerce() {
+		return Commerce;
+	}
+
+	public void setCommerce(Commerce commerce) {
+		Commerce = commerce;
+	}
 	
 }

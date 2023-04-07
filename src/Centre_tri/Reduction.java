011da08c9bbb4ea8fdfe7d7e8a11080d12e969f2
sylwan-bouderbala <sylwanbouderbala@outlook.fr>
2023-04-07 21:47:32 +0200

    Réductions

--- src/Centre_tri/Reduction.java
+++ src/Centre_tri/Reduction.java
@@ -1,33 +1,13 @@
-package net.codejava;
+package Centre_tri;
+
+import java.util.ArrayList;
 
 public class Reduction {
-public float taux;
-public String produit;
-public int pointsdefidélité;
-public Contrat contrat;
-
-public float getTaux() {return taux;}
-
-public void setTaux(float taux) {this.taux = taux;}
-
-public String getProduit() {return produit;}
-
-public void setProduit(String produit) {this.produit = produit;}
-
-public int getpointsdefidélité() {return pointsdefidélité;}
-
-public void setpointsdefidélité(int pointsdefidélité) {this.pointsdefidélité = pointsdefidélité;}
-
-public Reduction(float taux, String produit, Contrat contrat, int pointsdefidélité) {
-	this.taux = taux;
-	this.produit = produit;
-	this.pointsdefidélité = pointsdefidélité;
-	this.contrat = contrat;
-}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int Taux;
+	private ArrayList<String> Produits;
+	
+	public Reduction(int Taux,ArrayList<String> Produits) {
+		this.Taux = Taux;
+		this.Produits =  Produits;
 	}
-
 }

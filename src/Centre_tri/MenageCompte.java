96796e00b42637d26c58711f9a4651374f2c0441
solene abbas <thunderstom6a7@gmail.com>
2023-04-08 19:19:43 +0200

    menage

--- src/Centre_tri/MenageCompte.java
+++ src/Centre_tri/MenageCompte.java
@@ -12,8 +12,8 @@ import java.io.IOException;
 
 
 
-public class MenageCompte implements Serializable
-{
+public class MenageCompte implements Serializable {
+
 	private int identifiant;
 
 	private String motDePasse;
@@ -69,18 +69,19 @@ public class MenageCompte implements Serializable
 		return nvMenageCompte;
 	}
 
-	public void enregistrerMenageCompte(MenageCompte nouveauMenageCompte) {
-	    // Ouvrir le fichier CSV en mode écriture
-	    try (FileWriter writer = new FileWriter("compteMenage.csv", true)) { // true pour ajouter les données à la fin du fichier
-	        // Créer une ligne de données pour le nouvel objet MenageCompte
-	        String ligne = String.format("%d,%s,%s,%d,%d", nouveauMenageCompte.getIdentifiant(), nouveauMenageCompte.getmotdePasse(), nouveauMenageCompte.getQuartier(), nouveauMenageCompte.getnbPersonnes(), nouveauMenageCompte.getptsFidelite());
-	        // Écrire la ligne de données dans le fichier
-	        writer.write(ligne);
-	        writer.write(System.lineSeparator()); // saut de ligne pour faciliter la lecture
-	    } catch (IOException e) {
-	        System.out.printf("Erreur lors de l'enregistrement du fichier [compteMenage.csv] : [%s]%n%n", e.getMessage());
+	 public void enregistrerMenageCompte(MenageCompte nouveauMenageCompte) {
+	        // Ouvrir le fichier CSV en mode écriture
+	        try (FileWriter writer = new FileWriter("compteMenage.csv", true)) { // true pour ajouter les données à la fin du fichier
+	            // Créer une ligne de données pour le nouvel objet MenageCompte
+	            String ligne = String.format("%d,%s,%s,%d,%d", nouveauMenageCompte.getIdentifiant(), nouveauMenageCompte.getmotdePasse(), nouveauMenageCompte.getQuartier(), nouveauMenageCompte.getnbPersonnes(), nouveauMenageCompte.getptsFidelite());
+	            // Écrire la ligne de données dans le fichier
+	            writer.write(ligne);
+	            writer.write(System.lineSeparator()); // saut de ligne pour faciliter la lecture
+	            writer.flush(); // flush pour vider le buffer et écrire les données sur le disque
+	        } catch (IOException e) {
+	            System.out.printf("Erreur lors de l'enregistrement du fichier [compteMenage.csv] : [%s]%n%n", e.getMessage());
+	        }
 	    }
-	}
 
 	public void utiliser(int ptsFideliteAUtiliser)
 	{
@@ -113,7 +114,7 @@ public class MenageCompte implements Serializable
 	}
 	
 	public static void main(String[] args) {
-	    MenageCompte menage = new MenageCompte(1, "password", "Quartier A", 4, 10);
-	    menage.enregistrerMenageCompte(menage);
-	}
+        MenageCompte menage = new MenageCompte(1, "password", "Quartier A", 4, 10);
+        menage.enregistrerMenageCompte(menage);
+    }
 }

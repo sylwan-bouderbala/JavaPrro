a7c060188be04b847ade7c5c814394657f9b2842
sylwan-bouderbala <sylwanbouderbala@outlook.fr>
2023-04-08 14:15:10 +0200

    ajout inscription Réduc

--- src/Centre_tri/Contrat.java
+++ src/Centre_tri/Contrat.java
@@ -1,5 +1,8 @@
 package Centre_tri;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
@@ -24,10 +27,26 @@ public class Contrat {
 		this.setDuree(Duree);
 		this.setId(Id);
 	}
-	
+	//Fonction d'ecriture dans un fichier s
 	public void NouvelleReduction(ArrayList<String> Produits,int Taux) {
 		Reduction reduction = new Reduction(Taux,Produits);
 		reduction.setId(this.Reductions.size());
+		
+		String chaine = String.valueOf(this.Id) +";"+ String.valueOf(Taux)+";";
+		for (String elements : Produits) {
+			chaine = chaine + elements + "/;";
+		}
+		chaine = chaine + "\n";
+        try {
+            FileWriter writer = new FileWriter("reductions.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(chaine);
+            bufferedWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
 	}
 	
 	public void Renouveller(Date DateFin) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 	public static void Main(String argc[]) {
-		
+		int kalo = 14;
+		int mena = 32;
+		String chaine = String.valueOf(kalo) +";"+ String.valueOf(mena);
+        try {
+            FileWriter writer = new FileWriter("main.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(chaine);
+            bufferedWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
 	}
 }

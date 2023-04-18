37c309d9a2e918027cad9e00de51b1f7af45d5e2
sylwan-bouderbala <sylwanbouderbala@outlook.fr>
2023-04-18 13:09:17 +0200

    ajout creation poubelle

---
+++ src/com/cytech/Controller/AjoutPoubelleController.java
@@ -0,0 +1,32 @@
+package com.cytech.Controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class AjoutPoubelleController {
+	@FXML
+	private Button boutton;
+	
+	public void QuitPage(ActionEvent event) throws IOException {
+        // Code to execute on button click
+		String absolutePath = "C:/Users/sylwa/JavaPrro/src/Views/List_poubelles.fxml";
+		URL url = new File(absolutePath).toURI().toURL();
+		FXMLLoader loader = new FXMLLoader(url);
+	    Parent root = loader.load();
+	    Scene scene = new Scene(root);
+	    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	    stage.setScene(scene);
+	    stage.show();
+
+	}
+}
--- src/com/cytech/Controller/ListPoubellesController.java
+++ src/com/cytech/Controller/ListPoubellesController.java
@@ -1,5 +1,46 @@
 package com.cytech.Controller;
 
-public class ListPoubellesController {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+public class ListPoubellesController {
+	@FXML
+	private Button boutton;
+	@FXML
+	private Button boutton_quitter;
+	@FXML
+	private TableView<String> tableau;
+	public void AjoutPoubellePage(ActionEvent event) throws IOException {
+        // Code to execute on button click
+		String absolutePath = "C:/Users/sylwa/JavaPrro/src/Views/AjoutPoubelle.fxml";
+		URL url = new File(absolutePath).toURI().toURL();
+		FXMLLoader loader = new FXMLLoader(url);
+	    Parent root = loader.load();
+	    Scene scene = new Scene(root);
+	    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	    stage.setScene(scene);
+	    stage.show();
+
+	}
+	public void Quiter_page(ActionEvent event) throws IOException{
+		String absolutePath = "C:/Users/sylwa/JavaPrro/src/Views/HomePage.fxml";
+		URL url = new File(absolutePath).toURI().toURL();
+		FXMLLoader loader = new FXMLLoader(url);
+	    Parent root = loader.load();
+	    Scene scene = new Scene(root);
+	    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	    stage.setScene(scene);
+	    stage.show();
+	}
 }

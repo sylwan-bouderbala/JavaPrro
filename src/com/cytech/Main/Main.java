e91e02441418b29413b5af1eef143dd076c2c86c
sylwan-bouderbala <sylwanbouderbala@outlook.fr>
2023-04-20 18:33:46 +0200

    pb

--- src/com/cytech/Main/Main.java
+++ src/com/cytech/Main/Main.java
@@ -1,51 +1,27 @@
 package com.cytech.Main;
 
-import com.cytech.Centre_tri.centre_tri;
-import com.cytech.Usuelles.LectureFichier;
-
-/*package com.cytech.Main;
-import com.cytech.Test.*;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import com.cytech.Controller.*;
-import com.cytech.Menage.*;
-import com.cytech.Poubelle.Bac;
-import com.cytech.Poubelle.Poubelle;
-import javafx.scene.layout.AnchorPane;
-
-import javafx.collections.ObservableList;
-
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;*/
 
-public class Main /*extends Application*/ {
+public class Main extends Application {
 
-	public static void main(String[] args) {
-		centre_tri nouCentre_tri = new centre_tri("Los angee les", "centre de traitement intercommunal des Etrats",  "1234");
-		//System.out.println(nouCentre_tri.getIdentifiant());
-		LectureFichier lecteur = new LectureFichier("C:\\Users\\sylwa\\JavaPrro\\Centre_tri.csv");
-	}
-	/*  
-	private VBox page;
-	private Stage primaryStage;
+
 	@FXML
 	private ChoiceBox<String> choiceBox;
 	@FXML
 	private Button Button;
 	
-	// Pour Page de compte 
 	
-	
+
 	@Override
     public void start(Stage stage) throws Exception {
         // Load the FXML file
@@ -58,10 +34,8 @@ public class Main /*extends Application*/ {
         stage.setScene(scene);
         stage.show();
 
-        // Get the controller
         HomeController controller = loader.getController();
 
-        // Use the controller
         controller.initChoice();
     }
 	
@@ -69,13 +43,12 @@ public class Main /*extends Application*/ {
 	
 
 	
-	
     public static void main(String[] args) {
 
         launch(args);
 
     }
-*/
+
 	
 }
 

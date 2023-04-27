be7b03fb5f1e0dc6f8455b4e4ff144b7dd1b8cc5
Solène <120031563+rootcipher0@users.noreply.github.com>
2023-04-27 13:22:49 +0200

    Update PageDeCompteController.java

--- src/com/cytech/Controller/PageDeCompteController.java
+++ src/com/cytech/Controller/PageDeCompteController.java
@@ -1,64 +1,45 @@
 package com.cytech.Controller;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.time.LocalDate;
 
+import com.cytech.Centre_tri.centre_tri;
 import com.cytech.Poubelle.*;
-import com.cytech.Menage.MenageCompte;
-import com.cytech.Poubelle.Bac;
-import com.cytech.Poubelle.Poubelle;
-import com.cytech.Menage.CorbeilleOperation;
-import com.cytech.Main.Main;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
-import javafx.stage.Stage;
-import java.util.Scanner;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import java.io.FileNotFoundException;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.property.SimpleStringProperty;
+import com.cytech.Menage.*;
 
 
 public class PageDeCompteController {
+	private MenageCompte compte;
 	@FXML
     private TableView<Depot> DepotTable;
     @FXML
     private TableColumn<Depot, String> identifiantColumn;
     @FXML
     private TableColumn<Depot, String> quartierColumn;
+    @FXML
+    private TableColumn<Depot, String> quantiteColumn;
+    @FXML
+    private TableColumn<Depot, String> couleurColumn;
+    @FXML
+    private TableColumn<Depot, String> nbDechetColumn;
 
     @FXML
     private Label identifiantLabel;
     @FXML
-    private Label nbDechetsLabel;
+    private Label quartier;
     @FXML
     private Label quantiteLabel;
     @FXML
-    private Label valeurVerIfierLabel;
+    private Label nbDechetsLabel;
     @FXML
     private Label couleurBacUtilise;
     @FXML
-    private Label idDechet;
-    @FXML
-    private Label quartier;
+    private Label valeurVerIfierLabel;
+
     
 private HomeController homeController;
 
@@ -69,8 +50,11 @@ public PageDeCompteController() {
 @FXML
 private void initialize() {
 	// Initialize the person table with the two columns.
-    identifiantColumn.setCellValueFactory(cellData -> cellData.getValue().identifiantProperty());
-    quartierColumn.setCellValueFactory(cellData -> cellData.getValue().toString(getNbDechets()));
+    identifiantColumn.setCellValueFactory(cellData -> cellData.getValue().identifiantProperty().asString());
+    quartierColumn.setCellValueFactory(cellData -> cellData.getValue().quartierProperty());
+    quantiteColumn.setCellValueFactory(cellData -> cellData.getValue().quantiteProperty().asString());
+    couleurColumn.setCellValueFactory(cellData -> cellData.getValue().couleurProperty());
+    nbDechetColumn.setCellValueFactory(cellData -> cellData.getValue().nombreProperty().asString());
 }
 
 /**
@@ -82,46 +66,25 @@ public void setHomeController(HomeController homeController) {
     this.homeController = homeController;
 
     // Add observable list data to the table
-    depotTable.setItems(homeController.getDepotData());
+    DepotTable.setItems(homeController.getDepotData());
 }
 
+public MenageCompte getMenageCompte() {
+	return compte;
+}
+public void setMenageCompte(MenageCompte compte) {
+	this.compte = compte;
+}
 
-
+@FXML
+public void update() {
+	identifiant.setText("Bonjour" + this.compte.getIdentifiant());
+}
 @FXML
 public void gestionBouton(ActionEvent e)throws IOException {
 	
 }
-public void showOperationOverview() {
-    try {
-        // Load PageDeCompte
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("H:\\Documents\\GitHub\\JavaPrro\\src\\Views\\PageDeCompte.fxml"));
-        AnchorPane operationOverview = (AnchorPane) loader.load();
 
-        // Set PageDeCompte into the center of root layout.
-   //     rootLayout.setCenter(operationOverview);
-
-        // Give the controller access to the main.
-        PageDeCompteController controller = loader.getController();
-  //      controller.setMain(this);
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
-// Pour page de compte 
-
-public ObservableList<CorbeilleOperation> getOperationData() {
-	return operationData;
-}
-
-public void setMain(Main main) {
-  //  this.main = main;
-
-    // Add observable list data to the table
-//    CorbeilleOperationTable.setItems(main.getOperationData());
-    
-}
 
 
 }
